401f69cb1bbbd3c8e7428b058d4277e7a2dd5732
Dmitry <zadima@gmail.com>
2019-10-25 21:17:44 +0300

    button n5

--- src/components/HocsPlayground/Buttons/ButtonN5.js
+++ src/components/HocsPlayground/Buttons/ButtonN5.js
@@ -1,10 +1,33 @@
 /**
  * Необходимо имплементировать компонент и его логику путем композиции хоков и stateless компонента BaseButton
  */
-import {compose} from 'recompose';
-import BaseButton from './BaseButton';
+import React from 'react'
+import { juxt } from 'ramda'
+import { sample } from 'lodash'
+import { compose, withState, withHandlers, lifecycle } from 'recompose'
 
+import BaseButton from './BaseButton'
+
+const colors = ['green', 'blue', 'gray', 'red', 'purple', 'pink', 'orange', 'aliceblue', 'mediumaquamarine']
+
+const withRotateTransformation = Component => props => {
+  const transform = {
+    transform: `rotate(${props.degrees}deg)`,
+    display: 'inline-block'
+  }
+
+  return <span style={transform}><Component {...props} /></span>
+}
 
 export default compose(
-    //
+  withState('degrees', 'setDegrees', 0),
+  withHandlers({ onClick: ({ setDegrees }) => () => setDegrees(n => n + 30) }),
+  withRotateTransformation,
+  lifecycle({
+    componentDidUpdate() {
+      const { degrees, setOuterColor, setInnerColor } = this.props
+
+      degrees % 360 === 0 && juxt([setOuterColor, setInnerColor])(sample(colors))
+    }
+  })
 )(BaseButton)
